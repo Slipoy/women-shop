af709fd79842bdfe2f8dc7b6e9f4a9c88759c153
Vadim <slipoy.go@gmail.com>
2022-11-16 13:35:02 +0200

    test

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -13,7 +13,7 @@ const HomePage = ()=>{
                 <h2 className={style.headerRegalia}>Почему выбирают нас?</h2>
                 <div className={style.regaliaList}>
                     <p>Скидки постоянным клиентам от 5% <span className={style.circle}></span></p>
-                    <p>Предлагаем самые выгодные цены</p>
+                    <p>Предлагаем самые выгодные цены b</p>
                     <p>Наши покупатели всегда остаются довольны<span className={style.circle}></span></p>
                     <p>Ширикий ассортимент товаров для всей семьи</p>
                     <p>Возможность доставки в любой город Беларуси<span className={style.circle}></span> </p>
