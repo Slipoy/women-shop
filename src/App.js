c76e88c95cdd82e993fb0214308fe3cdde184561
Vadim <slipoy.go@gmail.com>
2022-11-16 14:00:13 +0200

    test

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@
 import './App.css'
 import Header from "./components/Header/Header";
 import HomePage from "./components/HomePage/HomePage";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-        <main>
-            <Header/>
-            <HomePage/>
-            <div className="semicircle"></div>
-        </main>
-    </div>
+      <BrowserRouter>
+          <div className="App">
+              <main>
+                  <Header/>
+                  <HomePage/>
+                  <div className="semicircle"></div>
+              </main>
+          </div>
+      </BrowserRouter>
+
   );
 }
 
